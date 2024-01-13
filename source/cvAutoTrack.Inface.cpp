a8b25af196dfb0e63c83e7d4a826625437024476
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2024-01-13 21:50:08 +0800

    update:调整越界错误

--- source/cvAutoTrack.Inface.cpp
+++ source/cvAutoTrack.Inface.cpp
@@ -17,7 +17,7 @@ int get_error_define_count()
 int get_error_define(int index, inface_string_ptr result)
 {
     if (index < 0 || index >= ErrorInvoker::m_locations.size())
-        return error("index out of range");
+        return error("error index out of range");
     auto &location = ErrorInvoker::m_locations[index];
     result->context = location.m_path + ":" + std::to_string(location.m_lineno) + ":" + std::to_string(location.m_colno) + " " + location.m_errmsg;
     return 0;
