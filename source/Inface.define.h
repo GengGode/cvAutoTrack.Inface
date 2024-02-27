3671517b7d58e4390126d5aedf881a0d10aea812
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2024-02-27 22:28:20 +0800

    refactor:全局变量移动到类内内联

--- source/Inface.define.h
+++ source/Inface.define.h
@@ -11,18 +11,17 @@ struct inface_string
 };
 typedef struct inface_string *inface_string_ptr;
 
-// task map
-inline static std::map<std::string, std::function<int(std::string)>> inface_task_map = {
+// task progress value
+class inface{
+public:
+inline static std::map<std::string, std::function<int(std::string)>> task_map = {
     {"download_start", nullptr},
     {"download_progress", nullptr},
     {"download_failed", nullptr},
     {"download_success", nullptr}};
-class inface{
-public:
 inline static std::map<std::string, std::function<int(int,int,std::string)>> progress_map = {
     {"download_progress", nullptr}};
-};
-inline static std::map<std::string, std::string> inface_value_map = {
+inline static std::map<std::string, std::string> value_map = {
     {"下载目录", "cache/"},
     {"加载目录", "core/"},
     {"依赖目录", "core/deps/"},
@@ -31,10 +30,11 @@ inline static std::map<std::string, std::string> inface_value_map = {
     {"项目链接", "https://download.api.weixitianli.com/cvAutoTrack.Core"},
     {"依赖链接", "https://download.api.weixitianli.com/cvAutoTrack.Core/DependFilesDownloadUrlAndHash"},
     {"强制使用本地版本", "false"}};
+};
 
 inline static std::string get_value(const std::string &key)
 {
-    return inface_value_map[key];
+    return inface::value_map[key];
 }
 template <typename... Args>
 inline static std::string get_value(const std::string &key, Args... args)
--- source/module.task.cpp
+++ source/module.task.cpp
@@ -4,14 +4,14 @@
 
 int get_task_callback_count()
 {
-    return inface_task_map.size();
+    return inface::task_map.size();
 }
 
 int get_task_callback_name(int index, inface_string_ptr result)
 {
-    if (index < 0 || index >= inface_task_map.size())
+    if (index < 0 || index >= inface::task_map.size())
         return error("回调表的索引超出范围");
-    auto iter = inface_task_map.begin();
+    auto iter = inface::task_map.begin();
     for (int i = 0; i < index; i++)
         iter++;
     result->context = iter->first;
@@ -23,7 +23,7 @@ int install_task_callback(const char *task_name, int (*callback)(const char * /*
     if (task_name == nullptr)
         return error("task_name是空指针");
 
-    inface_task_map[task_name] = [callback](std::string json) -> int
+    inface::task_map[task_name] = [callback](std::string json) -> int
     {
         return callback(json.c_str());
     };
@@ -35,6 +35,6 @@ int remove_task_callback(const char *task_name)
     if (task_name == nullptr)
         return error("task_name是空指针");
 
-    inface_task_map[task_name] = nullptr;
+    inface::task_map[task_name] = nullptr;
     return 0;
 }
